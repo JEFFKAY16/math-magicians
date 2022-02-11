7e008a0cd9fe1e1d3289dd880ee7647f69588600
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-02-11 13:59:52 +0200

    Add events to calculator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import Calculator from './components/Calculator';
+import calculate from './logic/calculate';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+  }
+
+  clickHandler = (event) => {
+    this.setState((initialState) => calculate(initialState, event));
   }
 
   render() {
     return (
       <div>
-        <Calculator />
+        <Calculator
+          total={this.state.total}
+          next={this.state.next}
+          operation={this.state.operation}
+          clickHandler={this.clickHandler}
+        />
       </div>
     );
   }
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,39 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import './Calculator.css';
+import Output from './Output';
 
 class Calculator extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
   render() {
+    const { total, next, operation } = this.props;
     return (
       <div className="calc-grid">
-        <div className="output" data-current-operation>0</div>
-        <button type="button" data-clear-all>AC</button>
-        <button type="button" data-change-sign>+/-</button>
-        <button type="button" data-percentage>%</button>
-        <button type="button" id="orange-background" data-operation>÷</button>
-        <button type="button" data-number>7</button>
-        <button type="button" data-number>8</button>
-        <button type="button" data-number>9</button>
-        <button type="button" id="orange-background" data-operation>×</button>
-        <button type="button" data-number>4</button>
-        <button type="button" data-number>5</button>
-        <button type="button" data-number>6</button>
-        <button type="button" id="orange-background" data-operation>-</button>
-        <button type="button" data-number>1</button>
-        <button type="button" data-number>2</button>
-        <button type="button" data-number>3</button>
-        <button type="button" id="orange-background" data-operation>+</button>
-        <button type="button" className="span-two" data-number>0</button>
-        <button type="button" data-number>.</button>
-        <button type="button" id="orange-background" data-equals>=</button>
+        <Output
+          total={total}
+          operation={operation}
+          next={next}
+        />
+        <button type="button" onClick={() => this.props.clickHandler('AC')} data-clear-all>AC</button>
+        <button type="button" onClick={() => this.props.clickHandler('+/-')} data-change-sign>+/-</button>
+        <button type="button" onClick={() => this.props.clickHandler('%')} data-percentage>%</button>
+        <button type="button" onClick={() => this.props.clickHandler('÷')} id="orange-background" data-operation>÷</button>
+        <button type="button" onClick={() => this.props.clickHandler('7')} data-number>7</button>
+        <button type="button" onClick={() => this.props.clickHandler('8')} data-number>8</button>
+        <button type="button" onClick={() => this.props.clickHandler('9')} data-number>9</button>
+        <button type="button" onClick={() => this.props.clickHandler('×')} id="orange-background" data-operation>×</button>
+        <button type="button" onClick={() => this.props.clickHandler('4')} data-number>4</button>
+        <button type="button" onClick={() => this.props.clickHandler('5')} data-number>5</button>
+        <button type="button" onClick={() => this.props.clickHandler('6')} data-number>6</button>
+        <button type="button" onClick={() => this.props.clickHandler('-')} id="orange-background" data-operation>-</button>
+        <button type="button" onClick={() => this.props.clickHandler('1')} data-number>1</button>
+        <button type="button" onClick={() => this.props.clickHandler('2')} data-number>2</button>
+        <button type="button" onClick={() => this.props.clickHandler('3')} data-number>3</button>
+        <button type="button" onClick={() => this.props.clickHandler('+')} id="orange-background" data-operation>+</button>
+        <button type="button" onClick={() => this.props.clickHandler('0')} className="span-two" data-number>0</button>
+        <button type="button" onClick={() => this.props.clickHandler('.')} data-number>.</button>
+        <button type="button" onClick={() => this.props.clickHandler('=')} id="orange-background" data-equals>=</button>
       </div>
 
     );
