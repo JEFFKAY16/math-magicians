f2506d9d01f3423ddc6dcbce3bf6d6aa3a3c01c1
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-02-16 17:37:56 +0200

    Add routers to full website

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import calculate from './logic/calculate';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import './components/Nav.css';
 
 class App extends React.Component {
   constructor() {
@@ -19,14 +24,26 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Calculator
-          total={this.state.total}
-          next={this.state.next}
-          operation={this.state.operation}
-          clickHandler={this.clickHandler}
-        />
-      </div>
+      <>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/calculator"
+              element={(
+                <Calculator
+                  total={this.state.total}
+                  next={this.state.next}
+                  operation={this.state.operation}
+                  clickHandler={this.clickHandler}
+                />
+              )}
+            />
+            <Route path="/quote" element={<Quote />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h2>Math Magicians</h2>
+      <br />
+      <p>It has survived not only five centuries, but also the leap into electronic.</p>
+      <p>Learn about the types of math on the SAT Math Test, when you should use a calculator.</p>
+      <p>College Board and National Merit Scholarship Corporation.</p>
+    </div>
+  );
+}
+
+export default Home;
